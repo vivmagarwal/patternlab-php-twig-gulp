627e50bdff834f48e9961fc874c2928700ebb40f
drupalastic <vivsurat@gmail.com>
2018-11-26 13:33:15 +0530

    Swiper component done. little refactoring required.

--- public/js/swiper.js
+++ public/js/swiper.js
@@ -11,6 +11,7 @@
  * Released on: November 1, 2018
  */
 
+
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
--- source/_patterns/02-organisms/04-swiper-slider/swiper-settings.js
+++ source/_patterns/02-organisms/04-swiper-slider/swiper-settings.js
@@ -1,8 +1,28 @@
 $(document).ready(function () {
-  //initialize swiper when document ready
+
   var mySwiper = new Swiper ('.swiper-container', {
     // Optional parameters
-    direction: 'vertical',
-    loop: true
-  });
+    //autoHeight: true,
+    speed: 1000,
+    direction: 'horizontal',
+    loop: true,
+
+    autoplay: {
+      delay: 4000,
+    },
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
+
+
 });
