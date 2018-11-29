b81c5b5ccee8db13406dd9be229516ce18f98d59
drupalastic <vivsurat@gmail.com>
2018-11-29 11:29:21 +0530

    Front page done, footer remaining.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,14 +1,3 @@
-function classToggle() {
-  this.classList.toggle('class1');
-  this.classList.toggle('class2');
-}
-
-document.querySelector('#div').addEventListener('click', classToggle);
-$(document).ready(function () {
-  $("#toggle-button").click(function () {
-    $("#toggle-me").toggle();
-  });
-});
 $(document).ready(function () {
   var mySwiper = new Swiper('.swiper-container', {
     // Optional parameters
